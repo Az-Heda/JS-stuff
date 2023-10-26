79b5d5fe741d7e0899678a105d90859cc9b63aad
Az-Heda <tecno19dragon@gmail.com>
2023-10-26 10:41:07 +0200

    -

---
+++ Server/index.js
@@ -0,0 +1,21 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// function get() {
+// 	let obj = {};
+// 	const filelist = fs.readdirSync(path.join(__dirname, 'moduli'));
+// 	for (let pos = 0; pos < filelist.length; pos ++) {
+// 		if (filelist[pos] !== '_.js') {
+// 			const requirePath = `./moduli/${filelist[pos].split('.')[0]}`;
+// 			obj[filelist[pos].split('.')[0]] = require(requirePath);
+// 		}
+// 	}
+// 	return obj
+// }
+// module.exports = get();
+
+return {
+	DownloadFile: require('./DownloadFile/downloadFile'),
+	WebServer: require('./WebServer/webserver'),
+	Sleep: require('./Sleep/sleep'),
+};
