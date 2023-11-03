7ee7995a8894ac1352d607e694e4fb51780ba91c
Az-Heda <tecno19dragon@gmail.com>
2023-11-03 12:23:57 +0100

    -

---
+++ Client/Socket/socket.js
@@ -0,0 +1,40 @@
+class MySocket {
+	/**
+	 * @param {str} url Url with protocol web socket
+	 * 
+	 * __Example__
+	 * ```js
+	 * new WebSocket('ws://localhost:5500')
+	 * new WebSocket('ws://127.0.0.1:8000')
+	 * ```
+	 */
+	constructor(url) {
+		this.url = url;
+		this.socket = new WebSocket(this.url);
+		this.history = [];
+	}
+
+	/**
+	 * @param {('message' | 'error' | 'close' | 'open')} type 
+	 * @param {function} f
+	 */
+	on(type, f) {
+		const valid = ['message', 'error', 'close', 'open'];
+		if (valid.includes(type)) {
+			this.socket.addEventListener(type, (type !== 'message') ?
+				f :
+				(event) => { f(event.data) }
+			);
+		}
+	}
+
+	/**
+	 * @param {str} name 
+	 * @param {object} o 
+	 */
+	send(name, o) {
+		let data = Object.assign({ _id: name }, { ...o });
+		this.history.push(data);
+		this.socket.send(JSON.stringify(data));
+	}
+}
---
+++ Client/Socket/socket.md
@@ -0,0 +1,27 @@
+# MySocket
+
+## Usage
+
+### Initialize
+```js
+const socket = new MySocket('ws://127.0.0.1:5500')
+```
+
+### Send data
+```js
+socket.send('<name>', { value: 'Object to send' })
+```
+
+### Get request history
+```js
+console.log(socket.history)	
+```
+
+### Add events
+```js
+socket.on('open',    () => { console.info('Connection is now open') });
+socket.on('close',   () => { console.info('Connection closed') });
+
+socket.on('message', (data) => { console.log(data) });
+socket.on('error',   (err) => { console.error(err) });
+```
