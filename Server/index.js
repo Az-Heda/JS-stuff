cf8ed81bb0655dbc27ca66d0c0c47611ca8bfe48
Az-Heda <tecno19dragon@gmail.com>
2023-11-09 17:25:13 +0100

    -

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,29 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// function get() {
-// 	let obj = {};
-// 	const filelist = fs.readdirSync(path.join(__dirname, 'moduli'));
-// 	for (let pos = 0; pos < filelist.length; pos ++) {
-// 		if (filelist[pos] !== '_.js') {
-// 			const requirePath = `./moduli/${filelist[pos].split('.')[0]}`;
-// 			obj[filelist[pos].split('.')[0]] = require(requirePath);
-// 		}
-// 	}
-// 	return obj
-// }
-// module.exports = get();
+console.clear();
 
-module.exports = {
-	DownloadFile: require('./DownloadFile/downloadFile'),
-	WebServer: require('./WebServer/webserver'),
-	Sleep: require('./Sleep/sleep'),
-};
+function get() {
+	let obj = {};
+	const filelist = fs.readdirSync(__dirname).map((item) => { return path.join(__dirname, item) });
+	for (let pos = 0; pos < filelist.length; pos ++) {
+		if (!filelist[pos].endsWith('_.js') && !filelist[pos].endsWith('index.js')) {
+			let files = fs.readdirSync(filelist[pos]);
+			let endpoint = path.join(filelist[pos], files.filter((item) => { return item.split('.').splice(-1)[0] == 'js'})[0].replace('.js', ''));
+			let endpointName = filelist[pos];
+			console.log(endpointName)
+			// obj[endpointName] = require('.'+endpoint.substr(__dirname.length).replaceAll('\\', '/'));
+			// const requirePath = `./${filelist[pos].split('.')[0]}/${fs.readdirSync(path.join(__dirname, filelist[pos]))}`;
+			// console.log(requirePath);
+			// obj[filelist[pos].split('.')[0]] = require(requirePath);
+		}
+	}
+	return obj
+}
+module.exports = get();
+
+// module.exports = {
+// 	DownloadFile: require('./DownloadFile/downloadFile'),
+// 	WebServer: require('./WebServer/webserver'),
+// 	Sleep: require('./Sleep/sleep'),
+// };
