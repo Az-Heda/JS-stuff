722d922d4144cf89c8bcf00815fb5e76e757dbbf
Az-Heda <139802979+Az-Heda@users.noreply.github.com>
2024-01-26 16:42:27 +0100

    -

--- test/event-stream/server.js
+++ test/event-stream/server.js
@@ -1,16 +1,18 @@
-const app        = require('express')();
+const app = require('express')();
 const index_html = require('path').join(__dirname, 'index.html');
 
 app.get('/', (req, res) => {
-    res.sendFile(index_html);
+	res.sendFile(index_html);
 });
 
 app.get('/data', (req, res) => {
-    res.header('Cache-Control', 'no-cache');
-    res.header('Content-Type', 'text/event-stream');
+	res.header('Cache-Control', 'no-cache');
+	res.header('Content-Type', 'text/event-stream');
 	setInterval(() => {
-		res.write(`x: ${new Date().toISOString(2).split('T').join(' - ').replace('Z', '')}\n\n`), 100;
-	});
+		res.write(`event: logger\n`), 100;
+		res.write(`id: loggerid\n`), 100;
+		res.write(`data: ${new Date().toISOString(2).split('T').join(' - ').replace('Z', '')}\n\n`), 100;
+	}, 200);
 });
 
 app.listen('7000');
