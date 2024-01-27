2e02802ab3491a143a5d1908a7756b523637685a
Az-Heda <139802979+Az-Heda@users.noreply.github.com>
2024-01-27 15:22:13 +0100

    -

--- Client/Loader/loader.js
+++ Client/Loader/loader.js
@@ -1,115 +1,82 @@
-class Loader {
-	static #_tag = document.createElement('div');
-	static #_css = document.createElement('style');
-	static #_theme = 'light';
-	static #_size = { w: 40, h: 40 };
-	static #_className = `cube-${Date.now()}`;
-	static #_colors = {
-		'light': { border: '#000000', background: '#00000040' },
-		'dark': { border: '#ffffff', background: '#ffffff40' },
+class Logger {
+	static #_status = {
+		'default': { bg: '#C084FC', color: '#000000' }
 	};
-	static #_sides = new Array(6)
-	.fill(0)
-	.map((_) => {
-		let side = document.createElement('div');
-		side.classList.add('side');
-		this.#_tag.appendChild(side);
-		return side;
-	});
-	
-	/**
-	 * @param {('light' | 'dark')} themeChecker Set the active theme
-	 */
-	static set darkTheme(themeChecker=false) {
-		return [this.#_theme, this.#_theme = (themeChecker == 0) ? 'light' : 'dark'][0]
+	static #_level = 0
+
+	// static default(text, lv = 1) {
+	// 	if (lv <= this.#_level) return;
+	// 	console.log(`%cLogger.${this.#_prepareName('default', false)} - ${this.#_getDate()}`, this.#_getStyles('#C084FC', '#000000'), text);
+	// }
+
+	static basicSetup() {
+		this.add('info', '#3b82f6');
+		this.add('success', '#a3e635');
+		this.add('warning', '#facc15');
+		this.add('error', '#ef4444');
+		this.add('LBlue', '#8be9fd');
+		this.add('pink', '#ff79c6');
+		this.add('Cyan', '#10b981');
+		this.add('purple', '#C084FC');
 	}
 
-	/**
-	 * @returns {boolean} Check wheather the Loader's Div element is in the DOM
-	 */
-	static get tagInBody() {
-		return document.body.contains(this.#_tag);
+	static add(name, bg, color = '#000000') {
+		if (!this.validateColor(bg)) throw new Error(`BG color is not valid`);
+		if (!this.validateColor(color)) throw new Error(`BG color is not valid`);
+		this.#_status[name] = { bg, color };
+		Object.defineProperty(this, this.#_prepareName(name, false), {
+			value: (text, lv = 1) => {
+				if (lv <= this.#_level) return;
+				console.log(`%cLogger.${this.#_prepareName(name, true)} - ${this.#_getDate()}`, this.#_getStyles(bg, color), text);
+			},
+			// writable: false,
+			// configurable: false,
+		})
 	}
 
-	/**
-	 * @returns {boolean} Check wheather the Loader's Style element is in the DOM
-	 */
-	static get cssInBody() {
-		return document.body.contains(this.#_css);
-	}
-	
-	/**
-	 * @returns {object} Colors for the current active theme
-	 */
-	static get themeColor() {
-		return this.#_colors[this.#_theme];
+	static #_getDate() {
+		let d = new Date();
+		let h = d.getHours().toString().padStart(2, 0);
+		let m = d.getMinutes().toString().padStart(2, 0);
+		let s = d.getSeconds().toString().padStart(2, 0);
+		let ms = d.getMilliseconds().toString().padStart(3, 0);
+		return `${h}:${m}:${s}.${ms}`;
 	}
 
-	/**
-	 * @returns {boolean} Returns true if all of the tags where succesfully removed from the DOM
-	 */
-	static get remove() {
-		if (this.tagInBody) { this.#_tag.remove(); }
-		if (this.cssInBody) { this.#_css.remove(); }
-		return true;
+	static #_prepareName(name, pad=false) {
+		let maxLength = (!pad) ? name.length : Object.keys(this.status).reduce((n, o) => Math.max(n, o.length), 0);
+		return (name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase()).padEnd(maxLength, ' ');
 	}
 
-	/**
-	 * @returns {{ tag: HTMLDivElement, style: HTMLStyleElement}} Returns the tags for the loader
-	 */
-	static get append() {
-		this.#_style;
-		if (this.#_tag) {
-			if (!this.#_tag.classList.contains(this.#_className)) {
-				this.#_tag.classList.add(this.#_className);
-			}
-			if (!this.tagInBody) {
-				document.body.appendChild(this.#_tag);
-			}
-		}
-		else {
-			throw new Error(`Loader not found`);
-		}
-		return { tag: this.tagInBody, style: this.cssInBody };
+	static #_getStyles(bg, color) {
+		if (!this.validateColor(bg)) throw new Error(`BG color is not valid`);
+		if (!this.validateColor(color)) throw new Error(`BG color is not valid`);
+		return Object.entries({
+			'padding': '5px 20px',
+			'line-height': '100%',
+			'font-weight': 'bold',
+			'background-color': bg,
+			'color': color,
+		}).map(i => i.join(':')).join('; ')
 	}
 
-	static get #_style() {
-		if (!this.cssInBody) {
-			document.body.appendChild(this.#_css);
-		}
-		this.#_css.innerHTML = `
-		/* Animation for Theme: ${this.#_theme} */
-		@keyframes AzHedaCubeAnimation {
-			0% { transform: rotate(45deg) rotateX(-25deg) rotateY(25deg); }
-			50% { transform: rotate(45deg) rotateX(-385deg) rotateY(25deg); }
-			100% { transform: rotate(45deg) rotateX(-385deg) rotateY(385deg); }
-		}
-		  
-		.${this.#_className} {
-			animation: AzHedaCubeAnimation 2s infinite ease;
-			transform-style: preserve-3d;
-			width: ${this.#_size.w}px;
-			height: ${this.#_size.h}px;
-			position: fixed;
-			top : calc(50% - ${this.#_size.h / 2}px);
-			left: calc(50% - ${this.#_size.w / 2}px);
-			z-index: 2147483647;
-		}
-		
-		.${this.#_className} div {
-			background-color: ${this.themeColor.background};
-			height: 100%;
-			position: absolute;
-			width: 100%;
-			border: 2px solid ${this.themeColor.border};
-		}
-		
-		.${this.#_className} div:nth-of-type(1) { transform: translateZ(-20px) rotateY(180deg); }
-		.${this.#_className} div:nth-of-type(2) { transform: rotateY(-270deg) translateX(50%); transform-origin: top right; }
-		.${this.#_className} div:nth-of-type(3) { transform: rotateY(270deg) translateX(-50%); transform-origin: center left; }
-		.${this.#_className} div:nth-of-type(4) { transform: rotateX(90deg) translateY(-50%); transform-origin: top center; }
-		.${this.#_className} div:nth-of-type(5) { transform: rotateX(-90deg) translateY(50%); transform-origin: bottom center; }
-		.${this.#_className} div:nth-of-type(6) { transform: translateZ(20px); }`;
+	static validateColor(hex) {
+		hex = hex.replace('#', '');
+		return (hex.length == 3 || hex.length == 6);
 	}
 
+	static get status() {
+		return this.#_status;
+	}
+
+	static get level() {
+		return this.#_level;
+	}
+
+	static set level(lv) {
+		if (typeof +lv == 'number' && !window.isNaN(+lv)) {
+			this.#_level = +lv;
+		}
+		return this.#_level;
+	}
 }
